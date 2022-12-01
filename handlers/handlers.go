bb6890ffddfb7a2b8fffc9a09318640d95a4dc62
najam awan <najamsk@gmail.com>
2022-12-01 14:21:13 +0200

    resthandlers haave all the http handlers, data and repo structs introduced

---
+++ handlers/handlers.go
@@ -0,0 +1,139 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"gorilla/internal/data"
+	"log"
+	"net/http"
+	"time"
+)
+
+type RestHandler struct {
+	Repo *data.Repo
+}
+
+func (h *RestHandler) SayNameMethod(w http.ResponseWriter, r *http.Request) {
+	log.Println("SayNameReal invoked")
+	w.Write([]byte("my name is real slim shady\n"))
+}
+
+func (h *RestHandler) MembersHandler(w http.ResponseWriter, r *http.Request) {
+	// swagger:route GET /members members listMembers
+	// Return a list of memebers from the database
+	// responses:
+	//	200: membersResponse
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(h.Repo.GetMembers())
+}
+
+func (h *RestHandler) NewMemberHandler(w http.ResponseWriter, r *http.Request) {
+	var newMember data.Member
+	rb := json.NewDecoder(r.Body)
+	err := rb.Decode(&newMember)
+	if err != nil {
+		log.Println("cant parse incoming data for member")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	newMember = h.Repo.CreateMember(newMember)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newMember)
+}
+
+func (h *RestHandler) NewTeamHandler(w http.ResponseWriter, r *http.Request) {
+	var n data.Team
+	rb := json.NewDecoder(r.Body)
+	err := rb.Decode(&n)
+	if err != nil {
+		log.Println("cant parse incoming data for team")
+		// w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	n = h.Repo.CreateTeam(n)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(n)
+}
+
+func (h *RestHandler) TeamsHandler(w http.ResponseWriter, r *http.Request) {
+	// swagger:route GET /teams teams listTeams
+	// Return a list of teams from the database
+	// responses:
+	//	200: teamsResponse
+
+	// repo := mystore.NewRepo()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(h.Repo.GetTeams())
+}
+
+func (h *RestHandler) StringHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("string handler invoked")
+	w.Write([]byte("Gorilla!\n"))
+}
+
+func (h *RestHandler) JsonStringHandler(w http.ResponseWriter, r *http.Request) {
+	j := `{"name": "najam awan", "email":"najamsk@gmail.com"}`
+	w.Write([]byte(j))
+}
+
+type SomeStruct struct {
+	Name  string
+	Email string
+}
+
+func (h *RestHandler) JsonStructHandler(w http.ResponseWriter, r *http.Request) {
+	data := SomeStruct{Name: "najam", Email: "najamsk@gmail.com"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}
+
+func (h *RestHandler) JsonMapHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Status Created"
+	resp["topic"] = "user/request"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+
+}
+
+func (h *RestHandler) LogHandler(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("logging middleware start")
+		next.ServeHTTP(w, r)
+		log.Println("logging middleware ends")
+	})
+}
+
+func (h *RestHandler) StreamHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("string handler invoked")
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		log.Println("responseWriter is not really a flusher")
+		return
+	}
+	//this header had no effect
+	w.Header().Set("Connection", "Keep-Alive")
+	//these two headers are needed to get the http chunk incremently
+	w.Header().Set("Transfer-Encoding", "chunked")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	for i := 0; i < 20; i++ {
+		// w.Write([]byte("Gorilla! \n"))
+		fmt.Println(i)
+		fmt.Fprintf(w, "Gorilla! %v \n", i)
+		flusher.Flush()
+		time.Sleep(1 * time.Second)
+		// time.Sleep(1 * time.Second)
+	}
+	fmt.Println("done")
+}
--- internal/data/data.go
+++ internal/data/data.go
@@ -3,11 +3,16 @@ package data
 import "fmt"
 
 type Repo struct {
-	teams   []Team
-	members []Member
+	// teams   []Team
+	// members []Member
+	db *DataStore
+}
+type DataStore struct {
+	Teams   []Team
+	Members []Member
 }
 
-func NewRepo() *Repo {
+func NewDataStore() *DataStore {
 	teams := []Team{
 		{1, "warwicks", nil},
 		{2, "maveriks", nil},
@@ -19,7 +24,22 @@ func NewRepo() *Repo {
 	teams[0].Leader = &members[0]
 	teams[1].Leader = &members[1]
 
-	return &Repo{teams: teams, members: members}
+	return &DataStore{Teams: teams, Members: members}
+}
+
+func NewRepo(db *DataStore) *Repo {
+	// teams := []Team{
+	// 	{1, "warwicks", nil},
+	// 	{2, "maveriks", nil},
+	// }
+	// members := []Member{
+	// 	{1, "najam awan", "nsa@gmail.com", 1},
+	// 	{2, "najaf awan", "najaf@gmail.com", 2},
+	// }
+	// teams[0].Leader = &members[0]
+	// teams[1].Leader = &members[1]
+
+	return &Repo{db: db}
 }
 
 // Member defines the structure for an API product
@@ -38,12 +58,24 @@ type Team struct {
 }
 
 func (r *Repo) GetTeams() []Team {
-	return r.teams
+	return r.db.Teams
 }
 
 func (r *Repo) GetMembers() []Member {
-	fmt.Println("data.members:", r.members)
-	return r.members
+	fmt.Println("data.members:", r.db.Members)
+	return r.db.Members
+}
+
+func (r *Repo) CreateMember(m Member) Member {
+	m.Id = len(r.db.Members) + 1
+	fmt.Println(m.Id)
+	r.db.Members = append(r.db.Members, m)
+	return m
+}
+func (r *Repo) CreateTeam(t Team) Team {
+	r.db.Teams = append(r.db.Teams, t)
+	t.Id = len(r.db.Teams)
+	return t
 }
 
 func init() {

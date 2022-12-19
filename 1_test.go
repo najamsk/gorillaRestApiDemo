ace7632ba988b330f3351c73e39b787c149acdcb
najam awan <najamsk@gmail.com>
2022-12-19 14:48:32 +0200

    adding fuzzy tests from google open source youtube talk video

--- 1_test.go
+++ 1_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gorilla/handlers"
 	"gorilla/internal/data"
 	"io"
@@ -10,6 +11,31 @@ import (
 
 	"go.uber.org/zap"
 )
+
+func Add(x, y int) int {
+	// if y == 0 {
+	// 	panic("y == 0")
+	// }
+	return x + y
+}
+func FuzzHandler(f *testing.F) {
+	tests := []struct {
+		x    int
+		y    int
+		want int
+	}{
+		{x: 1, y: 2, want: 3},
+		{x: 2, y: 6, want: 8},
+	}
+	for _, tt := range tests {
+		f.Add(tt.x, tt.y)
+	}
+	f.Fuzz(func(t *testing.T, x, y int) {
+		got := Add(x, y)
+		fmt.Println("got:", got)
+	})
+
+}
 
 func TestRootStringHandlerEndpoint(t *testing.T) {
 	logger, _ := zap.NewProduction()
@@ -38,5 +64,26 @@ func TestRootStringHandlerEndpoint(t *testing.T) {
 
 	if got != want {
 		t.Errorf("mismatch body with url:%s, want:%s, got:%s \n", url, want, got)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
